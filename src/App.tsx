6d16937cb8ae426941e5c388c760e74a0a269dc9
pvillor <pvillor@gmail.com>
2022-10-21 18:22:48 -0300

    feat: base header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+// import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div>
+    <>
+      <Header />
 
-    </div>
+      {/* <Footer /> */}
+    </>
   );
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { NavBar } from "./styles"
+
+const Header = () => {
+    return (
+        <NavBar>
+            <figure>
+                <img src="" alt="Motors shop" />
+            </figure>
+            <nav>
+                <figure>
+                    <img src="" alt="Nav" />
+                </figure>
+            </nav>
+        </NavBar>
+    )
+}
+
+export default Header
