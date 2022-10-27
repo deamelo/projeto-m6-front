3f2b6a91b007a0e564b943915b79fedd1d5e8bb4
pvillor <pvillor@gmail.com>
2022-10-27 11:04:02 -0300

    feat: global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Routes from "./routes";
+import Global from "./styles/global";
 
 function App() {
   return (
     <>
+      <Global />
       <Header />
       <Routes />
       <Footer />
